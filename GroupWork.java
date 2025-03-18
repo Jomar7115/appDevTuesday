e28c75ec3a368204afbef892eeec27a56c64edd8
Jomar <jrbendoy@paterostechnologicalcollege.edu.ph>
2025-03-18 16:19:09 +0800

    Updated Files

--- GroupWork.java
+++ GroupWork.java
@@ -10,38 +10,15 @@ public class GroupWork {
         int num2 = scanner.nextInt();
 
         // Calling methods from Calculator.java
-<<<<<<< HEAD
+
         System.out.println("Sum: " + Calculator.add(num1, num2)); // Group 1's method
+        System.out.println("Difference: " + Calculator.subtract(num1, num2)); // Group 2's method
         System.out.println("Quotient: " + Calculator.divide(num1, num2)); // Group 4 Method
         System.out.println("Remainder: " + Calculator.remainder(num1, num2)); // Group 5's Method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2)); // Group 10's method
-=======
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-
-        System.out.println("Max: " + max(num1, num2)); // Group 8's method
-
-
-
-        System.out.println("Cube: " + Calculator.cube(num1));  // Group 7's method
-        
-
-
-
         System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-
+        System.out.println("Cube: " + Calculator.cube(num1)); // Group 7's method
+        System.out.println("Max: " + Calculator.max(num1, num2)); // Group 8's method
+        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2)); // Group 10's method
         scanner.close();
     }
 }
-
-
-        System.out.println("Difference: " + Calculator.subtract(num1, num2));    // Group 2's method
-
-        System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4 Method
-        System.out.println("Remainder: " + Calculator.remainder(num1, num2));  // Group 5's Method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
->>>>>>> 9d98acc10463329c3457944189c0d3cef2f5a5af
-        scanner.close();
-    }
-}
-    
