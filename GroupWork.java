373b12cf765a54b68d307aa255c62d8d607f4328
Jomar <jrbendoy@paterostechnologicalcollege.edu.ph>
2025-03-18 16:42:38 +0800

    Modified and Final Copy wala ng hahabol bagsak na kung bagsak

--- GroupWork.java
+++ GroupWork.java
@@ -1,38 +1,23 @@
 import java.util.Scanner;
 
 public class GroupWork {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nCOLLAB CALCULATOR");
-        System.out.println("Enter first number: ");
-        int num1 = scanner.nextInt();
-        System.out.println("Enter second number: ");
-        int num2 = scanner.nextInt();
-<<<<<<< HEAD
-
-        // Calling methods from Calculator.java
-
-        System.out.println("Sum: " + Calculator.add(num1, num2)); // Group 1's method
-        System.out.println("Difference: " + Calculator.subtract(num1, num2)); // Group 2's method
-        System.out.println("Quotient: " + Calculator.divide(num1, num2)); // Group 4 Method
-        System.out.println("Remainder: " + Calculator.remainder(num1, num2)); // Group 5's Method
-        System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
-        System.out.println("Cube: " + Calculator.cube(num1)); // Group 7's method
-        System.out.println("Max: " + Calculator.max(num1, num2)); // Group 8's method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2)); // Group 10's method
-=======
-             // Calling methods from Calculator.java
-        System.out.println("Sum: " + Calculator.add(num1, num2));  // Group 1's method
-        System.out.println("Max: " + max(num1, num2)); // Group 8's method
-	System.out.println("Product: " + Calculator.multiply(num1, num2)); // Group 3's method
-        System.out.println("Cube: " + Calculator.cube(num1));  // Group 7's method
+        public static void main(String[] args) {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("\nCOLLAB CALCULATOR");
+                System.out.println("Enter first number: ");
+                int num1 = scanner.nextInt();
+                System.out.println("Enter second number: ");
+                int num2 = scanner.nextInt();
+                // Calling methods from Calculator.java
+                System.out.println("Sum: " + Calculator.add(num1, num2)); // Group 1's method
+                System.out.println("Difference: " + Calculator.subtract(num1, num2)); // Group 2's method
+                System.out.println("Product: " + Calculator.multiply(num1, num2)); // Group 3's method
+                System.out.println("Quotient: " + Calculator.divide(num1, num2)); // Group 4 Method
+                System.out.println("Remainder: " + Calculator.remainder(num1, num2)); // Group 5's Method
                 System.out.println("The square of " + num1 + " is: " + Calculator.square(num1)); // Group 6's method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
-        System.out.println("Difference: " + Calculator.subtract(num1, num2));    // Group 2's method
-        System.out.println("Quotient: " + Calculator.divide(num1, num2));  // Group 4 Method
-        System.out.println("Remainder: " + Calculator.remainder(num1, num2));  // Group 5's Method
-        System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2));  // Group 10's method
->>>>>>> 063d819edf84fec5a87deebe3882b775e8f6455a
-        scanner.close();
-    }
+                System.out.println("Cube: " + Calculator.kyub(num1)); // Group 7's method
+                System.out.println("Max: " + Calculator.max(num1, num2)); // Group 8's method
+                System.out.println("Are numbers equal? " + Calculator.isEqual(num1, num2)); // Group 10's method
+                scanner.close();
+        }
 }
